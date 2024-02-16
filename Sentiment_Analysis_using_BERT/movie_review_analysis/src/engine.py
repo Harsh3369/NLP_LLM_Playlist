b4d6f29550222eea6445776c33d440f78d3dfb1c
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2024-02-16 22:51:35 +0530

    added data science scripts

---
+++ Sentiment_Analysis_using_BERT/movie_review_analysis/src/engine.py
@@ -0,0 +1,8 @@
+from tqdm import tqdm
+
+def train_fn(data_loader, model, optimizer, device, accumulation_steps):
+    model.train()
+    pass
+
+def eval_fn():
+    pass
--- Sentiment_Analysis_using_BERT/movie_review_analysis/src/model.py
+++ Sentiment_Analysis_using_BERT/movie_review_analysis/src/model.py
@@ -19,3 +19,4 @@ class BERTBasedUncased(nn.Module):
         output = self.out(bo)
         return output
 
+    
