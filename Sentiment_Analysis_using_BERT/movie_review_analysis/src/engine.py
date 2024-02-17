a35734d064243e24795a12722b4882648914b0b0
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2024-02-17 21:01:51 +0530

    added eval function

--- Sentiment_Analysis_using_BERT/movie_review_analysis/src/engine.py
+++ Sentiment_Analysis_using_BERT/movie_review_analysis/src/engine.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 def loss_fn(outputs,targets):
     return nn.BCEWithLogitsLoss()(outputs,targets)
 
-def train_fn(data_loader, model, optimizer, device, accumulation_steps):
+def train_fn(data_loader, model, optimizer, device):
     model.train()
     
     for bi, d in tqdm(enumerate(data_loader), total= len(data_loader)):
@@ -26,5 +26,33 @@ def train_fn(data_loader, model, optimizer, device, accumulation_steps):
             token_type_ids=token_type_ids
         )
 
-def eval_fn():
-    pass
+        loss = loss_fn(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+def eval_fn(data_loader,model,device):
+    model.eval()
+    fin_targets = []
+    fin_outputs = []
+    with torch.no_grad():
+        for bi, d in tqdm(enumerate(data_loader), total= len(data_loader)):
+        ids = d["ids"]
+        mask = d["masks"]
+        token_type_ids = d["token_type_ids"]
+        targets = d["targets"]
+
+        ids = ids.to(device, dtype=torch.long)
+        mask = mask.to(device, dtype=torch.long)
+        token_type_ids = token_type_ids.to(device, dtype=torch.long)
+        targets = targets.to(device, dtype=torch.float)
+
+        outputs = model(
+            ids = ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        fin_targets.extend(targets.cpu().detach().numpy().tolist())
+        fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+
+    return fin_outputs, fin_targets
