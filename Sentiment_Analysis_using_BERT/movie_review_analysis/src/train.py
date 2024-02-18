2c03db072573ab0115220f85fa4268eb23938c89
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2024-02-18 22:48:18 +0530

    updated the functions

--- Sentiment_Analysis_using_BERT/movie_review_analysis/src/train.py
+++ Sentiment_Analysis_using_BERT/movie_review_analysis/src/train.py
@@ -1,7 +1,9 @@
 import config
 import dataset
 import torch
-import pandas as pd 
+import pandas as pd
+
+from model import BERTBasedUncased
 from sklearn import model_selection
 
 def run():
@@ -43,3 +45,13 @@ def run():
     )
 
     device = torch.device("cuda")
+    model = BERTBasedUncased()
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {'param': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
+        {'param': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+
+
