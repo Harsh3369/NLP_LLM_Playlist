d5a0c9eeffa26e17142f16945c99f8e42bb56b73
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2024-02-19 11:46:50 +0530

    added optimizer

--- Sentiment_Analysis_using_BERT/movie_review_analysis/src/train.py
+++ Sentiment_Analysis_using_BERT/movie_review_analysis/src/train.py
@@ -1,10 +1,16 @@
 import config
 import dataset
+import engine
 import torch
+from torch import nn
 import pandas as pd
+import numpy as np
 
 from model import BERTBasedUncased
-from sklearn import model_selection
+from sklearn import model_selection, metrics
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+
 
 def run():
     df = pd.read_csv(config.TRAINING_FILE).fillna("none")
@@ -53,5 +59,30 @@ def run():
         {'param': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
         {'param': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
+
+    num_train_steps = int(len(df_train)/ config.TRAIN_BATCH_SIZE * config.EPOCHS)
+    optimizer = AdamW(optimizer_parameters, lr = 3e-5)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_train_steps
+    )
+
+    # model = nn.DataParallel(model)
+
+    best_accuracy = 0
+    for epoch in range(config.EPOCHS):
+        engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        outputs, targets = engine.eval_fn(valid_data_loader, model, device)
+        outputs = np.array(outputs) >= 0.5
+        accuracy = metrics.accuracy_score(targets, outputs)
+        print(f"Accuracy Score = {accuracy}")
+        if accuracy > best_accuracy:
+            torch.save(model.state_dict(), config.MODEL_PATH)
+            best_accuracy = accuracy
+
+
+if __name__ == "__main__":
+    run()
 
 
